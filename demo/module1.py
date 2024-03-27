51740b7d14b9e628b67784202b6fb5cb41ff7928
David Baisas <davidbaisas96@gmail.com>
2024-03-26 20:17:33 -0400

    Module 09 in-class

---
+++ demo/module1.py
@@ -0,0 +1,13 @@
+def print_statement():
+    """
+    Purpose: This function prints a statement.
+    
+    INPUTS:
+    N/A
+    
+    OUTPUTS:
+    print statement
+    """
+    print("I hope I'm doing this right.")
+    
+    
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='0.1',
+    url='https://github.com/davidbaisas/mod09-in-class',
+    author='David Baisas',
+    author_email='db5kb@virginia.edu',
+    description='My module 9 demo package',
+    license='MIT',    
+    packages=['demo'],
+)
